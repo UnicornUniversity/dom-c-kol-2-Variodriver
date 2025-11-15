5be7e01fdb44e8949ae5b4c0a1f260e49bf16d67
Variodriver <59835749+Variodriver@users.noreply.github.com>
2025-11-15 18:45:50 +0100

    Removed TODOs

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-//TODO add imports if needed
-//import { exMain } from "./exclude/exampleAss2.js"
-//TODO add/change doc as needed
 /**
- * TODO - Write functional code for this application. You can call any other function, but usage of ".toString(numberSystem)" and "Number.parseInt(number, numberSystem)" is forbidden (only permitted when used on individual digits).
  * The main function which calls the application.
- * TODO - Please, add specific description here for the application purpose.
  * @param {string} inputNumber number that is being converted
  * @param {number} inputNumberSystem numerical system that the inputNumber is being converted from
  * @param {number} outputNumberSystem numerical system that the inputNumber is being converted into
@@ -15,7 +10,7 @@ export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
 
     let currentModifier = 1
     let currentNumberIndex = inputNumber.length;
-    
+
     while (currentNumberIndex > 0) {
         currentNumberIndex--;
         sum += currentModifier * Number.parseInt(inputNumber[currentNumberIndex])
@@ -26,7 +21,6 @@ export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
 }
 
 /**
- * TODO - Change this to contain all input number systems that your application can convert from.
  * Function which returns which number systems are permitted on input.
  * @returns {Array} array of numbers refering to permitted input systems
  */
@@ -35,7 +29,6 @@ export function permittedInputSystems() {
 }
 
 /**
- * TODO - Change this to contain all output number systems that your application can convert to.
  * Function which returns which number systems are permitted on output.
  * @returns {Array} array of numbers refering to permitted output systems
  */
